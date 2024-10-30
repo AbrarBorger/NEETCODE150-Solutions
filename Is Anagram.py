eacf0171357dd1a812b0f8aaecb8020a820f7330
AbrarBorger <abrarchowdhury@iut-dhaka.edu>
2024-10-30 12:50:11 +0600

    Optimized Valid Anagrams and learned how to use defaultdicts

--- Is Anagram.py
+++ Is Anagram.py
@@ -1,5 +1,27 @@
+from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = defaultdict(int)
+        
+        for x in s:
+            count[x] += 1
+            
+        for x in t:
+            count[x] -= 1
+            
+        for val in count.values():
+            if val != 0:
+                return False
+        
+        return True        
+
+# solution = Solution()
+# print(solution.isAnagram("racecar", "carrace"))  # Should return True
+# print(solution.isAnagram("hello", "world"))      # Should return False
+
         # sum = sum1 = sum2 = 0
         # ascii1 = [ord(char) for char in s]
         # ascii2 = [ord(char) for char in t]
@@ -15,22 +37,22 @@ class Solution:
         #     return False
         # s = "racecar"
         # t = "carrace"
-        t_list = list(t)
-        s_list = list(s)
+        # t_list = list(t)
+        # s_list = list(s)
         
-        if len(t_list) != len(s_list):
-            return False
+        # if len(t_list) != len(s_list):
+        #     return False
         
-        for i in s_list:
-            for j in t_list:
-                print(i, j)
-                if i == j:
-                    t_list.remove(j)
-                    break
+        # for i in s_list:
+        #     for j in t_list:
+        #         print(i, j)
+        #         if i == j:
+        #             t_list.remove(j)
+        #             break
                             
-        # print(len(t_list))
+        # # print(len(t_list))
             
-        if len(t_list) == 0:
-            return True
-        else:
-            return False
+        # if len(t_list) == 0:
+        #     return True
+        # else:
+        #     return False
