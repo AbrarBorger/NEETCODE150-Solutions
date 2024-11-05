106cb05ea9af5e57384fb9afde5b8f2a3e79a4c8
AbrarBorger <abrarchowdhury@iut-dhaka.edu>
2024-11-05 20:24:00 +0600

    Hashmaps

--- Two Sum.py
+++ Two Sum.py
@@ -1,11 +1,12 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if target - nums[i] == nums[j]:
-                    return i, j
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[nums[i]] = i
+
+        return []  
